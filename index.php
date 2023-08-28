36dd2887d6e6bdfcd1d8fffebf61eed5742b4514
Luca Weischenberg <l.weisch25@gmail.com>
2023-08-28 20:27:30 +0200

    The Basics
    The Basic Start of my first bigger Project. :)

---
+++ index.php
@@ -0,0 +1,14 @@
+<?php
+    include('header.php');
+?>
+<body>
+    <div class="navbar">
+        <ul>
+            <li><a href="#"><i class="fa-solid fa-house"></i>&nbsp;Home</a></li>
+            <li><a href="#"><i class="fa-solid fa-heart"></i>&nbsp;Quick Tips</a></li>
+            <li><a href="#"><i class="fa-solid fa-arrow-trend-up"></i>&nbsp;Self improvement</a></li>
+            <li id="login"><a href="loginpage.php"><i class="fa-solid fa-right-to-bracket"></i>&nbsp;Login</a></li>
+        </ul>
+    </div>
+</body>
+</html>
